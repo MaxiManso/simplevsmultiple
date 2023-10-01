a9ad7f39318365a9a08d7a75736a5aeb17944a4b
Manso Maximiliano Federico <maxifede20@gmail.com>
2023-10-01 19:32:43 -0300

    Update main.cpp

--- HerenciaMultiple/main.cpp
+++ HerenciaMultiple/main.cpp
@@ -2,6 +2,8 @@
 #include "dto/ConsolaDeJuego.cpp"
 #include "dto/DispositivoTactilPortatil.cpp"
 #include "dto/NintendoSwitch.cpp"
+#include "dto/PlayStation5.cpp"
+#include "dto/Ipad.cpp"
 using namespace std;
 
 
@@ -10,10 +12,9 @@ using namespace std;
 int main()
 {
     DispositivoTactilPortatil dis;
-
     dis.encender();
     dis.disminuirBrillo(20);
-    dis.copiar("BOCA JUNIORS EL MAS GRANDE");
+    dis.copiar("Prueba de  portapapeles");
     dis.pegar();
     dis.subirBrillo(60);
     dis.mostrarBateria();
@@ -34,7 +35,6 @@ int main()
 
     cout <<"***********************************************"<<endl;
 
-
     NintendoSwitch nin;
     nin.encender();
     nin.listarJuegos();
@@ -50,8 +50,22 @@ int main()
     nin.jugar();
     nin.cerrarJuego();
     nin.mostrarBateria();
+
+    nin.insertarCartucho("marioasdasdasdasd");
     nin.apagar();
+
     nin.jugar();
+
+    PlayStation5 ps5;
+    ps5.encender();
+    ps5.abrirJuego("FIFA 24");
+    ps5.insertarDisco("PES");
+    ps5.jugar();
+    ps5.sacarDisco();
+
+    Ipad ip;
+    ip.abrirAplicacion("WhatsApp");
+
 
     return 0;
 }
